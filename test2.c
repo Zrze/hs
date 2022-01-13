2a4b7b79f0eb8c7b59f6b799b3ef69e1f815ed5b
zrze <15522358560@163.com>
2022-01-13 17:20:56 +0800

    Initial commit ${DATE},${TIME}

---
+++ test2.c
@@ -0,0 +1,15 @@
+//
+// Createdon 2022/1/13.
+//
+
+#include<stdio.h>
+
+int main() {
+    int a =22;
+    printf("sssss\n");
+    printf("sssss\n");
+    printf("sssss\n");
+    printf("%d\n",a);
+    printf("sssss\n");
+    return 0;
+}
